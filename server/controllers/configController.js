8ec73ee77d4cecdff7449940d89300c1575d829a
James Tu <james@2-bit-toys.com>
2018-05-23 17:52:10 -0700

    pulling in saved keymaps from db and updating user interface

--- server/controllers/configController.js
+++ server/controllers/configController.js
@@ -11,10 +11,33 @@ const createConfig = (req, res) => {
       res.send('Internal error (saving to database)');
     } else {
       const config = dbRes.rows[0];
-      console.log('saved config: ', config);
+      // console.log('saved config: ', config);
       res.status(200).send('ok');
     }
   });
 };
 
-module.exports = { createConfig };
+const getConfig = (req, res) => {
+  const query =
+    `SELECT keymap FROM config WHERE user_id=${req.body.user_id};`;
+
+  // console.log('query ', query);
+
+  db.query(query, (dbErr, dbRes) => {
+    if (dbErr) {
+      // handle error
+      res.status(500);
+      res.send("Internal error (selecting from database)");
+    } else {
+
+      const config = dbRes.rows[0];
+      // console.log("saved config: ", config);
+      res.status(200);
+      res.json(config);
+    }
+  });
+};
+
+
+
+module.exports = { createConfig, getConfig };
