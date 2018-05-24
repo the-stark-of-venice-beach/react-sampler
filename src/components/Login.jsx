d5fb1c99307d29b94afde3d4be8d3b3357fed6e0
Jeff <jeffreyrkang@gmail.com>
2018-05-23 17:00:16 -0700

    Refactor from linter

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,23 +2,20 @@ import React from "react";
 import Sign from "./Sign.jsx";
 
 class Login extends React.Component {
-    constructor (props){
-      super(props);
-    }
-
-    render(){
-
-        return (
-          <div id="login-page">
-            {/*<img
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div id="login-page">
+        {/*<img
               id="login-background-image"
               src="https://i.ytimg.com/vi/AH8SCMMOZn8/maxresdefault.jpg"
             /> */}
-           <Sign clickHandler={this.props.clickHandler}/>
-          </div>
-        );
-      }
-    }
-    
-    
-    export default Login;
+        <Sign clickHandler={this.props.clickHandler} />
+      </div>
+    );
+  }
+}
+
+export default Login;
